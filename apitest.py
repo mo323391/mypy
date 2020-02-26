2c3733b2a1677a410f3c5c49bf5a7546d7a3d11f
mo323391 <61504271+mo323391@users.noreply.github.com>
2020-02-26 19:53:09 +0530

    Update apitest.py

--- apitest.py
+++ apitest.py
@@ -1,28 +1,27 @@
-# importing the requests library 
-import requests 
-
-# api-endpoint 
-URL = "http://maps.googleapis.com/maps/api/geocode/json"
-
-# location given here 
-location = "delhi technological university"
-
-# defining a params dict for the parameters to be sent to the API 
-PARAMS = {'address':location} 
-
-# sending get request and saving the response as response object 
-r = requests.get(url = URL, params = PARAMS) 
-
-# extracting data in json format 
-data = r.json() 
-
-
-# extracting latitude, longitude and formatted address 
-# of the first matching location 
-latitude = data['results'][0]['geometry']['location']['lat'] 
-longitude = data['results'][0]['geometry']['location']['lng'] 
-formatted_address = data['results'][0]['formatted_address'] 
-
-# printing the output 
-print("Latitude:%s\nLongitude:%s\nFormatted Address:%s"
-	%(latitude, longitude,formatted_address)) 
+import requests
+url = 'http://ES_search_demo.com/document/record/_search?pretty=true'
+data = '''{
+  "query": {
+    "bool": {
+      "must": [
+        {
+          "text": {
+            "record.document": "SOME_JOURNAL"
+          }
+        },
+        {
+          "text": {
+            "record.articleTitle": "farmers"
+          }
+        }
+      ],
+      "must_not": [],
+      "should": []
+    }
+  },
+  "from": 0,
+  "size": 50,
+  "sort": [],
+  "facets": {}
+}'''
+response = requests.post(url, data=data)
