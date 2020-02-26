e1d26dd84cd43d9cc14783c99edce7ded2b234a8
mo323391 <61504271+mo323391@users.noreply.github.com>
2020-02-26 20:01:34 +0530

    Update apitest.py

--- apitest.py
+++ apitest.py
@@ -1,3 +1,25 @@
-import xml.etree.ElementTree as ET
-tree = ET.parse('me.xml')
-root = tree.getroot()
+from xml.dom import minidom
+
+# parse an xml file by name
+mydoc = minidom.parse('me.xml')
+
+items = mydoc.getElementsByTagName('name')
+
+# one specific item attribute
+print('Item #2 attribute:')
+print(items[1].attributes['name'].value)
+
+# all item attributes
+print('\nAll attributes:')
+for elem in items:
+    print(elem.attributes['name'].value)
+
+# one specific item's data
+print('\nItem #2 data:')
+print(items[1].firstChild.data)
+print(items[1].childNodes[0].data)
+
+# all items data
+print('\nAll item data:')
+for elem in items:
+    print(elem.firstChild.data)
